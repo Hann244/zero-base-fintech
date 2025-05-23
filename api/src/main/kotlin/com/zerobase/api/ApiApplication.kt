8f80af525e79b44e0c700d3a3e10779ece9d5c88
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-05-24 00:44:07 +0900

    feat: domain - api 연결

--- api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
+++ api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
@@ -1,9 +1,17 @@
 package com.zerobase.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EntityScan(basePackages = ["com.zerobase.domain"])
+@ComponentScan(basePackages = ["com.zerobase"])
+@EnableJpaRepositories(basePackages = ["com.zerobase.domain.repository"])
+@EnableCaching
 class ApiApplication
 
 fun main(args: Array<String>) {

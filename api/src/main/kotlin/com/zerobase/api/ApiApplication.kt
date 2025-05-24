85711c60b528c024e98031289685c0963a8394cc
[CLOUD4] 한현 <gusgus1467@naver.com>
2025-05-24 16:06:31 +0900

    refac: 어노테이션 수정

--- api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
+++ api/src/main/kotlin/com/zerobase/api/ApiApplication.kt
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EntityScan(basePackages = ["com.zerobase.domain"])
 @ComponentScan(basePackages = ["com.zerobase"])
-@EnableJpaRepositories(basePackages = ["com.zerobase.domain.repository"])
 @EnableCaching
 class ApiApplication
 
